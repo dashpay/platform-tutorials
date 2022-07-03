751135cd41a3c12884d3df45ec5fbb1ecb7015df
AJ ONeal <coolaj86@gmail.com>
2022-07-02 19:18:12 -0600

    lint: add use strict to use es5+

--- connect.js
+++ connect.js
@@ -1,5 +1,7 @@
+"use strict";
+
 // See https://dashplatform.readme.io/docs/tutorial-connecting-to-testnet
-const Dash = require('dash');
+const Dash = require("dash");
 
 const client = new Dash.Client();
 
@@ -8,6 +10,6 @@ async function connect() {
 }
 
 connect()
-  .then((d) => console.log('Connected. Best block hash:\n', d))
-  .catch((e) => console.error('Something went wrong:\n', e))
+  .then((d) => console.log("Connected. Best block hash:\n", d))
+  .catch((e) => console.error("Something went wrong:\n", e))
   .finally(() => client.disconnect());
