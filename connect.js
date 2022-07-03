50642ed4709545b57e59c7db00b758c22b0ef65e
AJ ONeal <coolaj86@gmail.com>
2022-07-02 19:19:08 -0600

    docs: add default parameters for testnet
    I'm assuming people will want to choose testnet or mainnet. I updated the example to be self-documenting

--- connect.js
+++ connect.js
@@ -1,13 +1,21 @@
 // See https://dashplatform.readme.io/docs/tutorial-connecting-to-testnet
-const Dash = require('dash');
+const Dash = require("dash");
 
-const client = new Dash.Client();
+const client = new Dash.Client({
+  seeds: [
+    {
+      host: "seed-1.testnet.networks.dash.org",
+      httpPort: 3000,
+      grpcPort: 3010,
+    },
+  ],
+});
 
 async function connect() {
   return await client.getDAPIClient().core.getBestBlockHash();
 }
 
 connect()
-  .then((d) => console.log('Connected. Best block hash:\n', d))
-  .catch((e) => console.error('Something went wrong:\n', e))
+  .then((d) => console.log("Connected. Best block hash:\n", d))
+  .catch((e) => console.error("Something went wrong:\n", e))
   .finally(() => client.disconnect());
