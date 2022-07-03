8303233deb3a1fc4e6cac7f56e187b6097e33446
AJ ONeal <coolaj86@gmail.com>
2022-07-02 19:31:46 -0600

    feat: show how to use mnemonic and storage

--- create-wallet.js
+++ create-wallet.js
@@ -1,34 +1,52 @@
+"use strict";
+
+require("dotenv").config({ path: ".env" });
+
 // See https://dashplatform.readme.io/docs/tutorial-create-and-fund-a-wallet
-const Dash = require('dash');
+const Dash = require("dash");
 
-const clientOpts = {
-  network: 'testnet',
-  wallet: {
-    mnemonic: null, // this indicates that we want a new wallet to be generated
-                    // if you want to get a new address for an existing wallet
-                    // replace 'null' with an existing wallet mnemonic
-    offlineMode: true,  // this indicates we don't want to sync the chain
-                        // it can only be used when the mnemonic is set to 'null'
-  },
-};
+const Mnemonic = require("./lib/mnemonic.js");
+const store = require("./lib/store.js").create({
+  filepath: "./db.json",
+  namespace: "dash",
+});
 
-const client = new Dash.Client(clientOpts);
+async function main() {
+  const clientOpts = {
+    network: "testnet",
+    wallet: {
+      adapter: store,
+      // `null` would indicates that we want a new wallet to be generated
+      mnemonic: await Mnemonic.getOrCreate(process.env.MNEMONIC),
+      offlineMode: true, // this indicates we don't want to sync the chain
+      // it can only be used when the mnemonic is set to 'null'
+    },
+  };
 
-const createWallet = async () => {
+  const client = new Dash.Client(clientOpts);
+  // Handle wallet async errors
+  client.on("error", (error, context) => {
+    console.error(`Client error: ${error.name}`);
+    console.error(context);
+  });
+
+  let err = await createWallet(client).catch(function (e) {
+    return e;
+  });
+  client.disconnect();
+  if (err) {
+    throw err;
+  }
+}
+
+async function createWallet(client) {
   const account = await client.getWalletAccount();
 
-  const mnemonic = client.wallet.exportWallet();
   const address = account.getUnusedAddress();
-  console.log('Mnemonic:', mnemonic);
-  console.log('Unused address:', address.address);
-};
+  console.log("Unused address:", address.address);
+}
 
-createWallet()
-  .catch((e) => console.error('Something went wrong:\n', e))
-  .finally(() => client.disconnect());
-
-// Handle wallet async errors
-client.on('error', (error, context) => {
-  console.error(`Client error: ${error.name}`);
-  console.error(context);
+main().catch(function (err) {
+  console.error("Something went wrong:");
+  console.error(err.stack);
 });
