1c6ff1210fd1dfca64c279682be329621b4363c5
thephez <thephez@users.noreply.github.com>
2023-09-14 12:41:34 -0400

    feat: add example retrieving contract history
    Also includes register/update for a contract with history

---
+++ 2-Contracts-and-Documents/contract-history.js
@@ -0,0 +1,25 @@
+const Dash = require('dash');
+const {
+  default: loadDpp,
+  DashPlatformProtocol,
+} = require('@dashevo/wasm-dpp');;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Dash.Client({ network: process.env.NETWORK });
+loadDpp();
+const dpp = new DashPlatformProtocol(null);
+
+const retrieveContract = async () => {
+  const contractId = process.env.CONTRACT_ID; // Your contract ID
+  return await client.platform.contracts.history(contractId, 0, 10, 0);
+};
+
+retrieveContract()
+  .then((d) => {
+    Object.entries(d).forEach(([key, value]) => {
+      dpp.dataContract.createFromObject(value).then((contract) => console.dir(contract.toJSON(), {depth: 5}))
+    });
+  })
+  .catch((e) => console.error('Something went wrong:\n', e))
+  .finally(() => client.disconnect());
