2b5d0205930807a12ebabc1ea84ac2b01b1985a8
thephez <thephez@users.noreply.github.com>
2023-10-10 09:51:35 -0400

    refactor: remove wasm-dpp
    DPP is included in SDK so wasm-dpp isn't required

--- 2-Contracts-and-Documents/contract-retrieve-history.js
+++ 2-Contracts-and-Documents/contract-retrieve-history.js
@@ -1,25 +1,20 @@
 const Dash = require('dash');
-const {
-  default: loadDpp,
-  DashPlatformProtocol,
-} = require('@dashevo/wasm-dpp');;
 const dotenv = require('dotenv');
 dotenv.config();
 
 const client = new Dash.Client({ network: process.env.NETWORK });
-loadDpp();
-const dpp = new DashPlatformProtocol(null);
 
 const retrieveContractHistory = async () => {
   const contractId = process.env.CONTRACT_ID; // Your contract ID
-  return await client.platform.contracts.history(
-    contractId, 0, 10, 0);
+  return await client.platform.contracts.history(contractId, 0, 10, 0);
 };
 
 retrieveContractHistory()
   .then((d) => {
     Object.entries(d).forEach(([key, value]) => {
-      dpp.dataContract.createFromObject(value).then((contract) => console.dir(contract.toJSON(), {depth: 5}))
+      client.platform.dpp.dataContract
+        .createFromObject(value)
+        .then((contract) => console.dir(contract.toJSON(), { depth: 5 }));
     });
   })
   .catch((e) => console.error('Something went wrong:\n', e))
