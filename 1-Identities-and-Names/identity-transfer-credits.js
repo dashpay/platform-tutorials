3b643850d08f5a1c277e3aebc466594deda96ef4
thephez <thephez@users.noreply.github.com>
2024-01-10 15:32:10 -0500

    refactor: fix name

--- 1-Identities-and-Names/identity-transfer-credits.js
+++ 1-Identities-and-Names/identity-transfer-credits.js
@@ -18,17 +18,17 @@ const transferCredits = async () => {
   const identityId = process.env.IDENTITY_ID; // Your identity ID
   const identity = await client.platform.identities.get(identityId);
 
-  const recipientID = process.env.RECIPIENT_ID; // Recipient's ID
-  const recipientIdentity = await client.platform.identities.get(recipientID);
+  const recipientId = process.env.RECIPIENT_ID; // Recipient's ID
+  const recipientIdentity = await client.platform.identities.get(recipientId);
   console.log('Recipient identity balance before transfer: ', recipientIdentity.balance);
   const transferAmount = 1000; // Number of credits to transfer
 
   await client.platform.identities.creditTransfer(
     identity,
-    recipientID,
+    recipientId,
     transferAmount,
   );
-  return client.platform.identities.get(recipientID);
+  return client.platform.identities.get(recipientId);
 };
 
 transferCredits()
