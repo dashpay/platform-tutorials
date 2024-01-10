03d1c13e2493ad2ab3479221e3b63db2ecc89004
thephez <thephez@users.noreply.github.com>
2024-01-10 15:17:34 -0500

    refactor: display recipient balance before and after

--- 1-Identities-and-Names/identity-transfer-credits.js
+++ 1-Identities-and-Names/identity-transfer-credits.js
@@ -1,4 +1,4 @@
-// See https://docs.dash.org/projects/platform/en/stable/docs/tutorials/identities-and-names/transfer-credits.html
+// See https://docs.dash.org/projects/platform/en/stable/docs/tutorials/identities-and-names/transfer-credits-to-an-identity.html
 const Dash = require('dash');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -17,20 +17,21 @@ const client = new Dash.Client(clientOpts);
 const transferCredits = async () => {
   const identityId = process.env.IDENTITY_ID; // Your identity ID
   const identity = await client.platform.identities.get(identityId);
-  console.log('Identity balance before transfer: ', identity.balance);
 
   const recipientID = process.env.RECIPIENT_ID; // Recipient's ID
-  const transferAmount = 10; // Number of credits to transfer
+  const recipientIdentity = await client.platform.identities.get(recipientID);
+  console.log('Recipient identity balance before transfer: ', recipientIdentity.balance);
+  const transferAmount = 1000; // Number of credits to transfer
 
   await client.platform.identities.creditTransfer(
     identity,
     recipientID,
     transferAmount,
   );
-  return client.platform.identities.get(identityId);
+  return client.platform.identities.get(recipientID);
 };
 
 transferCredits()
-  .then((d) => console.log('Identity balance after transfer: ', d.balance))
+  .then((d) => console.log('Recipient identity balance after transfer: ', d.balance))
   .catch((e) => console.error('Something went wrong:\n', e))
   .finally(() => client.disconnect());
