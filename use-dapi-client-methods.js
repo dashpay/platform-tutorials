ce56f3844ce96e0a8a7ef477425474a5c3fc3946
thephez <thephez@users.noreply.github.com>
2024-07-22 14:48:04 -0400

    fix: update dapi client to comment out disabled methods

--- use-dapi-client-methods.js
+++ use-dapi-client-methods.js
@@ -6,9 +6,10 @@ const client = setupDashClient();
 
 async function dapiClientMethods() {
   console.log(await client.getDAPIClient().core.getBlockHash(1));
-  console.log(await client.getDAPIClient().core.getBestBlockHash());
-  console.log(await client.getDAPIClient().core.getBlockByHeight(1));
-  await client.getDAPIClient().core.getMasternodeStatus();
+  console.log(await client.getDAPIClient().core.getBestBlockHash())
+  // console.log(await client.getDAPIClient().core.getBlockByHeight(1)); // Disabled in Platform v1.0
+  console.log(await client.getDAPIClient().core.getBestBlockHeight());
+  // await client.getDAPIClient().core.getMasternodeStatus(); // Disabled in Platform v1.0
 
   return client.getDAPIClient().core.getBlockchainStatus();
 }
