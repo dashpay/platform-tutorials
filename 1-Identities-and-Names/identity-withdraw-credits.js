e5e9b209c2868b7b2ede926f5d35bf4fb8a36998
thephez <thephez@users.noreply.github.com>
2024-10-09 16:33:21 -0400

    feat: credit withdrawal tutorial

--- 1-Identities-and-Names/identity-withdraw-credits.js
+++ 1-Identities-and-Names/identity-withdraw-credits.js
@@ -1,4 +1,3 @@
-// See https://docs.dash.org/projects/platform/en/stable/docs/tutorials/identities-and-names/transfer-credits-to-an-identity.html
 const setupDashClient = require('../setupDashClient');
 
 const client = setupDashClient();
@@ -7,25 +6,31 @@ const withdrawCredits = async () => {
   const identityId = process.env.IDENTITY_ID; // Your identity ID
   const identity = await client.platform.identities.get(identityId);
 
-  const recipientId = process.env.RECIPIENT_ID; // Recipient's ID
-  const recipientIdentity = await client.platform.identities.get(recipientId);
   console.log(
-    'Recipient identity balance before transfer: ',
-    recipientIdentity.balance,
+    'Identity balance before transfer: ',
+    identity.balance,
   );
-  const transferAmount = 300000; // Number of credits to transfer
 
-  await client.platform.identities.creditTransfer(
-    identity,
-    recipientId,
-    transferAmount,
-  );
-  return client.platform.identities.get(recipientId);
+  const toAddress = process.env.WITHDRAWAL_ADDRESS; // Destination Dash address
+  const withdrawalAmount = 190000; // Number of credits to withdraw
+
+  console.log('Starting credit withdrawal...');
+  
+  // Temporarily force minRelay to have a value so withdrawal succeeds
+  // https://github.com/dashpay/platform/issues/2233
+  client.wallet.storage.getDefaultChainStore().state.fees.minRelay = 1000
+  // console.log(client.wallet.storage.getDefaultChainStore().state.fees.minRelay)
+
+  const metadata = await client.platform.identities.withdrawCredits(identity, withdrawalAmount, {
+    toAddress,
+  });
+  console.log(metadata)
+  return client.platform.identities.get(identityId);
 };
 
 withdrawCredits()
   .then((d) =>
-    console.log('Recipient identity balance after transfer: ', d.balance),
+    console.log('Identity balance after withdrawal: ', d.balance),
   )
   .catch((e) => console.error('Something went wrong:\n', e))
   .finally(() => client.disconnect());
