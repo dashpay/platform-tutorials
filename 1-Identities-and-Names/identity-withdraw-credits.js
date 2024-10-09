2deccb163fc2987f72af24c45e3ac7d2c91cbea5
thephez <thephez@users.noreply.github.com>
2024-10-09 16:42:34 -0400

    style: run npm fmt

--- 1-Identities-and-Names/identity-withdraw-credits.js
+++ 1-Identities-and-Names/identity-withdraw-credits.js
@@ -6,31 +6,30 @@ const withdrawCredits = async () => {
   const identityId = process.env.IDENTITY_ID; // Your identity ID
   const identity = await client.platform.identities.get(identityId);
 
-  console.log(
-    'Identity balance before transfer: ',
-    identity.balance,
-  );
+  console.log('Identity balance before transfer: ', identity.balance);
 
   const toAddress = process.env.WITHDRAWAL_ADDRESS; // Destination Dash address
   const withdrawalAmount = 190000; // Number of credits to withdraw
 
   console.log('Starting credit withdrawal...');
-  
+
   // Temporarily force minRelay to have a value so withdrawal succeeds
   // https://github.com/dashpay/platform/issues/2233
-  client.wallet.storage.getDefaultChainStore().state.fees.minRelay = 1000
+  client.wallet.storage.getDefaultChainStore().state.fees.minRelay = 1000;
   // console.log(client.wallet.storage.getDefaultChainStore().state.fees.minRelay)
 
-  const metadata = await client.platform.identities.withdrawCredits(identity, withdrawalAmount, {
-    toAddress,
-  });
-  console.log(metadata)
+  const metadata = await client.platform.identities.withdrawCredits(
+    identity,
+    withdrawalAmount,
+    {
+      toAddress,
+    },
+  );
+  console.log(metadata);
   return client.platform.identities.get(identityId);
 };
 
 withdrawCredits()
-  .then((d) =>
-    console.log('Identity balance after withdrawal: ', d.balance),
-  )
+  .then((d) => console.log('Identity balance after withdrawal: ', d.balance))
   .catch((e) => console.error('Something went wrong:\n', e))
   .finally(() => client.disconnect());
