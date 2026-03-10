8b8b53e2ce3156b16fb771ffdd8544ae4c8f0083
thephez <thephez@users.noreply.github.com>
2026-03-10 12:58:28 -0400

    chore: whitespace cleanup

--- 1-Identities-and-Names/name-search-by-name.mjs
+++ 1-Identities-and-Names/name-search-by-name.mjs
@@ -22,7 +22,7 @@ try {
   });
 
   for (const [id, doc] of results) {
-    console.log(doc.toJSON())
+    console.log(doc.toJSON());
     const { label, parentDomainName } = doc.toJSON();
     console.log(`${label}.${parentDomainName} (ID: ${id.toString()})`);
   }
